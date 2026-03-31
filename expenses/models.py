9e005c37df75289f046e8bc713ac97064662705a
Colin <colinleonghongseng@gmail.com>
2026-03-31 17:53:43 +0800

    added expense model

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Expense(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    amount = models.FloatField()
+    category = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='expenses'
+    )
+
